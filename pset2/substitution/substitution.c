9a2544f9e94f67dcd13dca1c370792992d5bd6d7
xStanlee <57445724+xStanlee@users.noreply.github.com>
2020-02-23 19:17:59 +0100

    Create substitution.c

---
+++ pset2/substitution/substitution.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+#include <ctype.h>
+
+int main (void) {
+
+        string key = get_string("Put the key on: ");
+        int n = strlen(key);
+
+            while(n != 26) {
+            printf("KEY IS INVALID! IT HAS TO HAVE 26 LENGTH OF LETTERS\n");
+            key = get_string("Try again! :");
+            n = strlen(key);
+        }
+
+            for (int i = 0; i < n; i++){
+                while(!isalpha(key[i])) {
+                printf("KEY IS INVALID! SHOULD BE BUILD ONLY BY LETTERS\n");
+                key = get_string("Try again! :");
+            }
+        }
+            for(int i = n; i >= 0; i--){
+                for(int j = i-1; j >= 0; j--){
+                while(!(key[i] != key[j])){
+                    printf("KEY IS INVALID! YOU DUPLICATE LETTER SOMEWHERE\n");
+                    key = get_string("Try again! :");
+                }
+                }
+            }
+
+            for (int i = 0; i < n; i++){
+                key[i] = tolower(key[i]);
+            }
+
+            string transformFrom = "abcdefghijklmnopqrstuvwxyz";
+            int tFF = strlen(transformFrom);
+            string plaintext = get_string("Put the text to encrypt: ");
+            int plainL = strlen(plaintext);
+            char signs[5] = {' ', ',', '.', '!', '?'};
+
+            char temporary[plainL];
+            for (int i = 0 ; i < plainL; i++){
+                for(int j = 0; j < tFF; j++){
+
+                    if(plaintext[i] == transformFrom[j]){
+                        temporary[i] = key[j];
+                    }
+                    else
+                    {
+                        for(int k = 0 ; k < 5;  k++)
+                        {
+                            if(plaintext[i] == signs[k])
+                            {
+                                temporary[i] = signs[k];
+                            }
+
+
+                    }
+              }
+      } 
+
+}
+
+
+printf("%s\n", temporary);
+}

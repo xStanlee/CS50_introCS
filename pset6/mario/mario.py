827d9e0bc4fbec27f7c807d64c4527bc3f02d9b4
xStanlee <57445724+xStanlee@users.noreply.github.com>
2020-03-02 10:29:24 +0100

    Create mario.py

---
+++ pset6/mario/mario.py
@@ -0,0 +1,20 @@
+# Littlebit sphagetthi mario
+
+from cs50 import get_int
+
+n = get_int("Put the hight of the pyramid object: \n")
+l = 1
+m = n-1
+for i in range(n):
+    if m>=0 or n>=k:
+        for h in range(m):
+            print(" ", end = '')
+        for k in range(l):
+            print("#", end = '')
+        for j in [0]:
+            print(" ", end = '')
+        for k in range(l):
+            print("#", end = '')
+        l += 1
+        m -= 1
+        print(end = '\n')

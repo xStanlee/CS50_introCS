658aad851d306b3e0a74e7e98afd5d61b90a25ca
xStanlee <57445724+xStanlee@users.noreply.github.com>
2020-03-11 07:39:05 +0100

    Create 4.sql

---
+++ pset7/movies/4.sql
@@ -0,0 +1,10 @@
+// Shows 10.0 ratings film (was curious)
+
+/*SELECT ratings.rating, ratings.votes
+FROM movies
+INNER JOIN ratings ON movies.id = ratings.movie_id WHERE ratings.rating = 10;
+*/
+
+SELECT COUNT(rating) AS Counter
+FROM ratings
+WHERE rating = 10;

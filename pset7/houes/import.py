ff8b5c00404f274ed4870bb4eca59bd8de5e5a6b
xStanlee <57445724+xStanlee@users.noreply.github.com>
2020-03-14 12:23:14 +0100

    CSV to DB
    Taking CSV file filter it and push to DB

--- pset7/houes/import.py
+++ pset7/houes/import.py
@@ -1,1 +1,45 @@
-# INIT
+import csv
+import sys
+from cs50 import SQL
+
+db = SQL ("sqlite:///students.db")
+strCharacters = str(sys.argv[1])
+NULL = None
+my_Sql_push = ''
+
+if strCharacters != NULL:
+    with open(strCharacters, 'r', encoding='ISO-8859-1') as csvB:
+        #csvBase = csv.reader(csvB)
+        csvBase = csv.DictReader(csvB)
+        for row in csvBase:
+            ### Creating new keys inside Dictionary
+            row['middle'] = ''
+            row['last'] = ''
+
+            ### Part fullname
+            split = row['name'].split( );
+
+            row['name'] = split[0]
+
+            if len(split) == 3:
+                row['middle'] = split[1]
+                row['last'] = split[2]
+
+            else:
+                row['middle'] = None
+                row['last'] = split[1]
+
+            f_name = row['name']
+            s_name = row['middle']
+            l_name = row['last']
+            b = row['birth']
+            h = row['house']
+
+            my_Sql_push = f"""INSERT INTO students (first, middle , last, house, birth)
+            VALUES
+            ('{f_name}', '{s_name}', '{l_name}', '{h}', '{b}') """
+
+            ### Push to DB
+            db.execute(my_Sql_push)
+
+    csvB.close()

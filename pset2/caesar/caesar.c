13fab065c448045400fef616b8f8d9608c25e432
xStanlee <57445724+xStanlee@users.noreply.github.com>
2020-03-14 15:24:36 +0100

    Update caesar.c

--- pset2/caesar/caesar.c
+++ pset2/caesar/caesar.c
@@ -1,31 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <cs50.h>
 #include <string.h>
 
 
 
-int main (int key, string argc[]){
+int main (int argc, char *argv[]){
 
-    //int key = get_int("Key to encode: ");
+    if (argc != 2)
+    {
+        printf("Usage: ./Caesar [ENCRYPT]\n");
+        return 1;
+    }
+
     string plaintext = get_string("Put text to encrypt: ");
     int n = strlen(plaintext);
-
+    int crypt = atoi(argv[1]);
     char realCiphertext[n + 1];
     int ciphertext[n];
 
     for (int i = 0; i < n; i++) {
         ciphertext[i] = (int) plaintext[i];
         if(ciphertext[i] != 44){
-        ciphertext[i] += key;
+        ciphertext[i] += crypt;
         } else {
         ciphertext[i] = (int) plaintext[i];
         }
-        //ciphertext[i] = ciphertext[i] % 26;
         realCiphertext[i] = (char)ciphertext[i];
     }
         realCiphertext[n + 1] = '\0';
 
-    //printf("%d\n", plaintext[0]);
-    printf("%s\n", realCiphertext);
-    printf("%d\n", key);
+    printf("Output encrypted text: %s\n", realCiphertext);
 }

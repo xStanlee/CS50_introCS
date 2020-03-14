e2b01678757177520deba6aaa581418d7465070c
xStanlee <57445724+xStanlee@users.noreply.github.com>
2020-03-14 13:39:20 +0100

    Last exercise from 1st week

--- pset1/credit.c
+++ pset1/credit.c
@@ -10,8 +10,8 @@
         long cardValue = card;
         long visa = card;
         long count = 0;
-       
-                while(valueOfCard != 0){    
+
+                while(valueOfCard != 0){
                 valueOfCard /= 10;
                 count++;
             }
@@ -32,26 +32,23 @@
                     }
                 }
             }
-            printf("%ld\n", fullValidation);
             if (fullValidation % 2 == 0 && (count == 13 || count == 15 || count == 16)) {
                 printf("CARD STATUS: VALID\n");
                 if(count == 15 && (cardValue == 34 || cardValue == 37)){
                 printf("TYPE OF CARD: AMEX\n");
-                } 
-                else if (count == 16 && 
-                (cardValue == 51 || cardValue == 52 || 
-                cardValue == 53 || cardValue == 54 || 
+                }
+                else if (count == 16 &&
+                (cardValue == 51 || cardValue == 52 ||
+                cardValue == 53 || cardValue == 54 ||
                 cardValue == 55)){
                 printf("TYPE OF CARD: MASTERCARD\n");
-                } else if (visa == 4 && (count == 13 || count == 16)){   
-                printf("TYPE OF CARD: VISA\n");    
+                } else if (visa == 4 && (count == 13 || count == 16)){
+                printf("TYPE OF CARD: VISA\n");
                 } else {
-                printf("SOMETHING WENT WRONG!\n");    
-                } 
-                printf("%ld\n", card);
+                printf("SOMETHING WENT WRONG!\n");
+                }
             }else {
                 printf("CARD STATUS: INVALID\n");
                 printf("%ld\n", card);
             }
     }
-
